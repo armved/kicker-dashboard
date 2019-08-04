048452c8ecd6d7855ff0758e0977a057c67ec54c
Avetik Harutyunyan <avetik.harutyunyan@logicify.com>
2019-08-04 22:26:47 +0300

    Add react router and base templates

---
+++ src/components/Matches/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Matches = () => (
+  <h1>Matches Page</h1>
+)
+
+export default Matches;
---
+++ src/components/Players/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Players = () => (
+  <h1>Players Page</h1>
+)
+
+export default Players;
